85c91481fafa325a6be557a4107d1714adcd4748
Paul Chechetin <paulche@yandex.ru>
2013-03-06 10:08:27 +0400

    Complete lab4

---
+++ lab4/README.md
@@ -0,0 +1,4 @@
+## To see result
+```
+action!
+```
---
+++ lab4/action.m
@@ -0,0 +1,11 @@
+function action()
+
+  % Draw plot
+  x = 0.1:0.1:4;
+  y = f;
+  H = plot(x,y(x));
+
+  dsolution = d_solve(1,2,15)
+  nsolution = n_solve(15,1.1)
+  fzero_solution = fzero(f,[1 2])
+end
