590a75eef3bf9c6a88d3e758f1a9b280aaf343e3
Paul Chechetin <paulche@yandex.ru>
2013-03-06 13:38:41 +0400

    Repair iter_method

--- lab5/iter_method.m
+++ lab5/iter_method.m
@@ -6,9 +6,9 @@ function [solution serial] = iter_method(A,b,x,n)
   [B c] = iter(A,b);
 
   for i = 1:n
-    r = B * x + c;
-    serial(i+1,:) = r;
+    x = B * x + c;
+    serial(i+1,:) = x;
   end
 
-  solution = r;
+  solution = x;
 end
